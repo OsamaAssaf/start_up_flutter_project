e6085e9cb11eb47222a27da24e3db1adc5d96285
OsamaAssaf <osama.assaf.o@gmail.com>
2023-12-11 14:00:13 +0300

    update

--- lib/resources/constants_manager.dart
+++ lib/resources/constants_manager.dart
@@ -9,17 +9,19 @@ class ConstantsManager {
 
   static const String googlePlayLink =
       'https://play.google.com/store/apps/details?id=com.example.start_up_workspace';
-  static const String appStoreLink = 'http://apps.apple.com/<country>/app/<app–name>/id<store-ID>';
+  static const String appStoreLink =
+      'http://apps.apple.com/<country>/app/<app–name>/id<store-ID>';
+  static const String appStoreId = 'appStoreId';
   static const String shareText =
-      'Introducing \'App Name\' app – your pocket companion for mental wellness. If you or someone you care about could use support, download now from Google Play and the App Store.\nAndroid Edition: $googlePlayLink\nIOS Edition: $appStoreLink';
+      '\'App Name\'. If you or someone you care about could use support, download now from Google Play and the App Store.\nAndroid Edition: $googlePlayLink\nIOS Edition: $appStoreLink';
   static const String termsAndConditionsUrl = 'https://google.com';
 }
 
 class DotenvManager {
-  static final String appStoreId = dotenv.env['APP_STORE_ID']!;
   static final String languagePrefsKey = dotenv.env['LANGUAGE_PREFS_KEY']!;
   static final String themeModePrefsKey = dotenv.env['THEME_MODE_PREFS_KEY']!;
-  static final String isDarkThemePrefsKey = dotenv.env['IS_DARK_THEME_PREFS_KEY']!;
+  static final String isDarkThemePrefsKey =
+      dotenv.env['IS_DARK_THEME_PREFS_KEY']!;
 
   static final String _domain = dotenv.env['DOMAIN']!;
   static final String _mainApiPath = dotenv.env['API_PATH']!;
